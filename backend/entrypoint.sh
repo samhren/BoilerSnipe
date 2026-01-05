b7efeeee508287f13a31cbf785cfd280a337b64a
Sam Hren <samaldenhren@gmail.com>
2026-01-05 15:33:46 -0500

    force v6

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default to port 8000 if PORT is not set
-PORT=${PORT:-8000}
+PORT=${PORT:-8080}
 
 # Start the application
-exec uvicorn app.main:app --host 0.0.0.0 --port "$PORT" --proxy-headers --forwarded-allow-ips '*'
+exec uvicorn app.main:app --host :: --port "$PORT" --proxy-headers --forwarded-allow-ips '*'
