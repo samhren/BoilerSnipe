e07ef8c049b15208f880e08f06857bf572157dd5
Sam Hren <samaldenhren@gmail.com>
2026-01-06 22:30:18 -0500

    umami

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -24,7 +24,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           src: "/assets/lib.js",
           'data-host-url': "/"
         }}
-        debug={true}
         onlyInProduction={false}
       />
     </GoogleOAuthProvider>
